fcd98c20599a46866615a680498e2e74b9b0c7bc
Atulya Singh <atulyarajsingh@gmail.com>
2025-09-12 16:39:42 -0400

    Uploading assn02

---
+++ src/assn02/LMS.java
@@ -0,0 +1,49 @@
+package assn02;
+import java.util.Scanner;
+
+public class LMS {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            // Display menu
+            System.out.println("Library Menu:");
+            System.out.println("0. Exit");
+            System.out.println("1. Add a Book");
+            System.out.println("2. Display All Books");
+            System.out.println("3. Check Out a Book");
+            System.out.println("4. Return a Book");
+            System.out.println("Enter your choice:");
+
+            int n = sc.nextInt();
+            sc.nextLine(); // consume newline
+
+            if (n == 0) {
+                System.out.println("Bye for now!");
+                break; // exit loop
+            }
+
+            if (n == 1) {
+                String b = sc.nextLine();
+                String A = sc.nextLine();
+                book.add_book(b, A);
+            }
+            else if (n == 2) {
+                book.display_All(book.books);
+            }
+            else if (n == 3) {
+                int h = sc.nextInt() - 1; // convert to 0-based index
+                book.check_out_book(h);
+            }
+            else if (n == 4) {
+                int y = sc.nextInt() - 1; // convert to 0-based index
+                book.return_book(y);
+            }
+            else {
+                System.out.println("Invalid choice. Try again.");
+            }
+        }
+
+        sc.close();
+    }
+}
